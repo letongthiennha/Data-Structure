d0b7ca9bd12b8e32e04668686008a55925b7efcd
Lê Tống Thiện Nhân <letongthiennha@gmail.com>
2025-03-24 23:00:03 +0700

    Button Done

--- Data Structure/Button.h
+++ Data Structure/Button.h
@@ -1,38 +1,73 @@
-//#include "raylib.h"
-//#include "GUIObject.h"
-//#include<string>
-//class Button : public GUIObject {
-//private:
-//    Rectangle hitBox;
-//    float width, height;
-//    std::string text;
-//
-//    Color idleColor=BLACK;
-//    Color hoverColor=YELLOW;
-//    Color activeColor=RED;
-//
-//    Color renderColor = idleColor;
-//    Vector2 textPos;
-//    int textSize=30;
-//    void ComputeTextPos();
-//    float activeTimer;      
-//    const float clickDuration = 0.17f;
-//public:
-//    // Constructor
-//    Button() = default;
-//    Button(float x, float y, float width, float height, std::string text);
-//    // Destructor
-//    virtual ~Button() = default;
-//    //Set Color
-//    void SetIdleColor(Color color);
-//    void SetHoverColor(Color color);
-//    void SetActiveColor(Color color);
-//    //Set Text
-//    void SetTextSize(int textSize);
-//    void SetTextPos(Vector2 TextPos);
-//    // Render
-//    void update();
-//    void render() override;
-//    bool isHover() const;
-//    bool isClicked() const;
-//};
+#pragma once
+#include "raylib.h"
+#include "GUIObject.h"
+#include<string>
+class Button  {
+private:
+    Rectangle hitBox;
+    bool isActive = true;
+    //State
+    bool isSelected = false;
+    Vector2 m_size;
+    Vector2 m_position;
+
+    //Color
+    Color m_idleColor=BLACK;
+    Color m_hoverColor=YELLOW;
+    Color m_activeColor=RED;
+    Color m_color;
+    //Text
+    int m_textSize=30;
+    std::string m_text="";
+
+    //Texture
+    Texture2D m_texture;
+public:
+
+
+    // Constructor
+    Button() = default;
+    Button(Vector2 position,Vector2 size, std::string text);
+
+
+    // Destructor
+     ~Button() = default;
+//====================================Color===========================
+    //Set Color
+    void SetIdleColor(Color color);
+    void SetHoverColor(Color color);
+    void SetActiveColor(Color color);
+    void SetColor(Color idle, Color Clicked, Color hover);
+
+
+    //====================================Text=====================
+    //Set Text
+    void setText(std::string, int textSize);
+    void SetTextSize(int textSize);
+
+    //Get Text
+    int getTextSize();
+
+    //================================Position And Size=====================
+    void setPosition(Vector2 position);
+    Vector2 getPosition();
+    void setSize(Vector2 size);
+
+    //=================================State===================
+    bool isHover() ;
+    bool isClicked() ;
+    void Select();
+    void unSelect();
+    void active();
+    void deactive();
+
+    //============================= Update======================
+    void update();
+    //=============================DrawShape=========================
+    void drawRectangle();
+    void drawRoundedRectangle(int roundness);
+    void drawOutline(int roundness, int segments, int thickness, Color color);
+
+    void drawTexture(Vector2 texturePosition);
+    void drawText(Color color);
+};
