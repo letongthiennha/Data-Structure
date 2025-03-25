1c34896c3913edae52269eac6d4a18a5c84163fc
xxavierr31 <anhkhoi25606@gmail.com>
2025-03-26 06:52:52 +0700

    Added ShortestPathCode.h

---
+++ Data Structure/ShortestPathCode.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+
+//temporary, readapt later
+
+const std::string addEdge = {
+    "graph[u][v] = 1;\n"
+    "graph[v][u] = 1;" 
+};
+
+const std::string removeEdge = {
+    "if (graph[u][v] == graph[v][u])\n" 
+        "graph[v][u] = 0;\n"
+    "graph[u][v] = 0;" 
+};
+
+const std::string setWeight = {
+    "if (graph[u][v] == graph[v][u])\n" 
+        "graph[v][u] = weight;\n"
+    "graph[u][v]= weight;"
+};
+
+const std::string toggleDirected = {
+    "graph[v][u] = 0;"
+};
+
+const std::string untoggleDirected = {
+    "if (graph[u][v] == graph[v][u]) return;\n"
+    "if (graph[u][v]) graph[v][u] = graph[u][v];"
+};
+
+const std::string Dijkstra = {
+    "distances[source] = 0;\n"
+    "pQueue.push(source);\n"
+    "while (!pQueue.empty()) {\n"
+    "    current = priorityQueue.pop();\n"
+    "    for (neighbor : graph.getNeighbors(current)) {\n"
+    "        if (distances[current] + weight < distances[neighbor]) {\n"
+    "            distances[neighbor] = distances[current] + weight;\n"
+    "            pQueue.push(neighbor);\n"
+    "        }\n"
+    "    }\n"
+    "}"
+};
